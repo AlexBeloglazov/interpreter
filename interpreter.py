31f3f805742d3ce6279433ad2187ba5df23528b8
nox <abelogla@buffalo.edu>
2017-03-20 10:25:42 -0400

    Added rest of the commands, if..else, let, bind support

--- interpreter.py
+++ interpreter.py
@@ -17,6 +17,28 @@ class Error:
     def __str__(self):
         return ":error:"
 
+class Unit:
+    def __str__(self):
+        return ":unit:"
+
+#### MIXINS ####
+
+class BinaryAriphmetic:
+    def get_operands(stack, env):
+        s, f = stack[-2:]
+        s = int(env[s.name]) if isinstance(s, Name) else int(s)
+        f = int(env[f.name]) if isinstance(f, Name) else int(f)
+        return s, f
+
+class BinaryLogic:
+    def get_operands(stack, env):
+        s, f = stack[-2:]
+        s = env[s.name].bool if isinstance(s, Name) else s.bool
+        f = env[f.name].bool if isinstance(f, Name) else f.bool
+        stack.pop()
+        stack.pop()
+        return s, f
+
 #### COMMANDS ####
 
 class CMDPush:
@@ -44,48 +66,40 @@ class CMDError:
     def execute(self, stack, env):
         stack.append(Error())
 
-class CMDAdd():
+class CMDAdd(BinaryAriphmetic):
     def execute(self, stack, env):
         try:
-            s, f = stack[-2:]
-            s = int(env[s.name]) if isinstance(s, Name) else int(s)
-            f = int(env[f.name]) if isinstance(f, Name) else int(f)
+            s,f = BinaryAriphmetic.get_operands(stack, env)
             stack.pop()
             stack.pop()
             stack.append(s + f)
         except:
             stack.append(Error())
 
-class CMDSub():
+class CMDSub(BinaryAriphmetic):
     def execute(self, stack, env):
         try:
-            s, f = stack[-2:]
-            s = int(env[s.name]) if isinstance(s, Name) else int(s)
-            f = int(env[f.name]) if isinstance(f, Name) else int(f)
+            s,f = BinaryAriphmetic.get_operands(stack, env)
             stack.pop()
             stack.pop()
             stack.append(s - f)
         except:
             stack.append(Error())
 
-class CMDMul():
+class CMDMul(BinaryAriphmetic):
     def execute(self, stack, env):
         try:
-            s, f = stack[-2:]
-            s = int(env[s.name]) if isinstance(s, Name) else int(s)
-            f = int(env[f.name]) if isinstance(f, Name) else int(f)
+            s,f = BinaryAriphmetic.get_operands(stack, env)
             stack.pop()
             stack.pop()
             stack.append(s * f)
         except:
             stack.append(Error())
 
-class CMDDiv():
+class CMDDiv(BinaryAriphmetic):
     def execute(self, stack, env):
         try:
-            s, f = stack[-2:]
-            s = int(env[s.name]) if isinstance(s, Name) else int(s)
-            f = int(env[f.name]) if isinstance(f, Name) else int(f)
+            s,f = BinaryAriphmetic.get_operands(stack, env)
             f = s // f
             stack.pop()
             stack.pop()
@@ -93,12 +107,10 @@ class CMDDiv():
         except:
             stack.append(Error())
 
-class CMDRem():
+class CMDRem(BinaryAriphmetic):
     def execute(self, stack, env):
         try:
-            s, f = stack[-2:]
-            s = int(env[s.name]) if isinstance(s, Name) else int(s)
-            f = int(env[f.name]) if isinstance(f, Name) else int(f)
+            s,f = BinaryAriphmetic.get_operands(stack, env)
             f = s % f
             stack.pop()
             stack.pop()
@@ -126,14 +138,105 @@ class CMDSwap:
             stack.append(f)
             stack.append(s)
 
+class CMDAnd(BinaryLogic):
+    def execute(self, stack, env):
+        try:
+            s, f = BinaryLogic.get_operands(stack, env)
+            stack.append(Bool(s and f))
+        except:
+            stack.append(Error())
+
+class CMDOr(BinaryLogic):
+    def execute(self, stack, env):
+        try:
+            s, f = BinaryLogic.get_operands(stack, env)
+            stack.append(Bool(s or f))
+        except:
+            stack.append(Error())
+
+class CMDNot:
+    def execute(self, stack, env):
+        try:
+            f = stack[-1]
+            f = Bool(not (env[f.name].bool if isinstance(f, Name) else f.bool ))
+            stack.pop()
+            stack.append(f)
+        except:
+            stack.append(Error())
+
+class CMDEqual(BinaryAriphmetic):
+    def execute(self, stack, env):
+        try:
+            s,f = BinaryAriphmetic.get_operands(stack, env)
+            stack.pop()
+            stack.pop()
+            stack.append(Bool(s == f))
+        except:
+            stack.append(Error())
+
+class CMDLessThan(BinaryAriphmetic):
+    def execute(self, stack, env):
+        try:
+            s,f = BinaryAriphmetic.get_operands(stack, env)
+            stack.pop()
+            stack.pop()
+            stack.append(Bool(s < f))
+        except:
+            stack.append(Error())
+
+class CMDBind:
+    def execute(self, stack, env):
+        try:
+            s, f = stack[-2:]
+            if isinstance(f, Error):
+                return stack.append(f)
+            if isinstance(f, Name):
+                f = env[f.name]
+            env[s.name] = f
+            stack.pop()
+            stack.pop()
+            stack.append(Unit())
+        except:
+            stack.append(Error())
+
+class CMDIf:
+    def execute(self, stack, env):
+        try:
+            t, s, f = stack[-3:]
+            t = env[t.name].bool if isinstance(t, Name) else t.bool
+            stack.pop()
+            stack.pop()
+            stack.pop()
+            stack.append(f if t else s)
+        except:
+            stack.append(Error())
+
 class CMDQuit:
     pass
+
+class CMDEnd:
+    pass
+
+class CMDLet():
+    def __init__(self):
+        self.body = []
+    def append(self, instr):
+        self.body.append(instr)
+    def execute(self, stack, env):
+        lstack = []
+        lenv = env.copy()
+        for cmd in self.body:
+            cmd.execute(lstack, lenv)
+        if lstack:
+            stack.append(lstack[-1])
 
 class Interpreter:
     def __init__(self,):
         self.cmdMap = {'pop': CMDPop(), ':true:': CMDTrue(), ':false:': CMDFalse(), ':error:': CMDError(), \
                         'add': CMDAdd(), 'sub': CMDSub(), 'mul': CMDMul(), 'div': CMDDiv(), 'rem': CMDRem(), \
-                        'neg': CMDNeg(), 'swap': CMDSwap(), 'quit': CMDQuit()}
+                        'neg': CMDNeg(), 'swap': CMDSwap(), 'quit': CMDQuit(), 'and': CMDAnd(), 'or': CMDOr(), \
+                        'not': CMDNot(), 'equal': CMDEqual(), 'lessThan': CMDLessThan(), 'bind': CMDBind(), \
+                        'if': CMDIf(), 'end': CMDEnd()}
     def parse_line(self, line):
         if (line.startswith("push")):
             arg = line.split("push")[-1].strip()
@@ -144,20 +247,28 @@ class Interpreter:
             elif arg.startswith('"') and arg.endswith('"'):
                 return CMDPush(arg[1:-1])
             return CMDError()
+        elif line == "let":
+            return CMDLet()
         return self.cmdMap.get(line, CMDError())
     def run(self, strIn, strOut):
         inFile = open(strIn, mode='r')
         listInstructions = [[]]
         for line in inFile.readlines():
             instruction = self.parse_line(line.strip())
-            if isinstance(instruction, CMDQuit):
+            if isinstance(instruction, CMDLet):
+                listInstructions[-1].append(instruction)
+                listInstructions.append(instruction)
+            elif isinstance(instruction, CMDEnd):
+                listInstructions.pop()
+            elif isinstance(instruction, CMDQuit):
                 break
-            listInstructions[-1].append(instruction)
+            else:
+                listInstructions[-1].append(instruction)
         inFile.close()
         stack, environment = [], {}
         for cmd in listInstructions[-1]:
             cmd.execute(stack, environment)
-        with open(strOut, mode='w', buffering=1000) as out:
+        with open(strOut, mode='w') as out:
             stack.reverse()
             for i in stack:
                 out.write(str(i) + "\n")
